e0f24dcb9bdf232b74bd438cfc28b789275cee41
Scott Smethurst <scott-smethurst@hotmail.com>
2018-09-19 19:59:48 +0100

    Extracted a ‘given’ step to tidy up the ApiGatewayStageSettings tests.

--- test/ApiStageSettings.js
+++ test/ApiStageSettings.js
@@ -4,19 +4,13 @@ const ApiStageSettings = require('../src/ApiStageSettings');
 const { expect } = require('chai');
 
 describe('Creating the settings for the API Gateway stage', () => {
-  let settings;
+  let serverless, options, settings;
 
-  describe('when nothing has been configured in the serverless.yml file', () => {
+  describe('when nothing has been configured in serverless.yml', () => {
 
     before(() => {
-      const serverless = {
-        service: {
-          provider: {
-            stage: 'dev',
-            region: 'eu-west-2'
-          }
-        },
-      };
+      serverless = given_a_serverless_yml_file();
+
       settings = new ApiStageSettings(serverless);
     });
 
@@ -25,22 +19,12 @@ describe('Creating the settings for the API Gateway stage', () => {
     });
   });
 
-  describe('enablling detailing CloudWatch metrics in the serverless.yml file', () => {
+  describe('enablling detailed CloudWatch metrics in serverless.yml', () => {
 
     before(() => {
-      const serverless = {
-        service: {
-          provider: {
-            stage: 'dev',
-            region: 'eu-west-2'
-          },
-          custom: {
-            apiGatewayCloudWatchSettings: {
-              metricsEnabled: true
-            }
-          }
-        }
-      };
+      serverless = given_a_serverless_yml_file();
+      serverless.service.custom.apiGatewayCloudWatchSettings = { metricsEnabled: true };
+
       settings = new ApiStageSettings(serverless);
     });
 
@@ -49,22 +33,11 @@ describe('Creating the settings for the API Gateway stage', () => {
     });
   });
 
-  describe('disabling detailing CloudWatch metrics in the serverless.yml file', () => {
+  describe('disabling detailed CloudWatch metrics in serverless.yml', () => {
 
     before(() => {
-      const serverless = {
-        service: {
-          provider: {
-            stage: 'dev',
-            region: 'eu-west-2'
-          },
-          custom: {
-            apiGatewayCloudWatchSettings: {
-              metricsEnabled: false
-            }
-          }
-        }
-      };
+      serverless = given_a_serverless_yml_file();
+      serverless.service.custom.apiGatewayCloudWatchSettings = { metricsEnabled: false };
 
       settings = new ApiStageSettings(serverless);
     });
@@ -73,4 +46,42 @@ describe('Creating the settings for the API Gateway stage', () => {
       expect(settings.metricsEnabled).to.be.false;
     });
   });
+
+  describe('when stage and region options are provided during an sls deploy', () => {
+
+    before(() => {
+
+      serverless = given_a_serverless_yml_file();
+      serverless.service.custom.apiGatewayCloudWatchSettings = { metricsEnabled: true };
+      options = { stage: 'test', region: 'us-east-1' };
+
+      settings = new ApiStageSettings(serverless, options);
+    });
+
+    it('uses the stage option', () => {
+      expect(settings.stage).to.equal(options.stage);
+    });
+
+    it('not use the provider stage', () => {
+      expect(settings.stage).to.not.equal(serverless.service.provider.stage);
+    });
+
+    it('uses the region option', () => {
+      expect(settings.region).to.equal(options.region);
+    });
+
+    it('not use the provider region', () => {
+      expect(settings.region).to.not.equal(serverless.service.provider.region);
+    });
+  });
+});
+
+const given_a_serverless_yml_file = () => ({
+  service: {
+    provider: {
+      stage: 'dev',
+      region: 'eu-west-2'
+    },
+    custom: {}
+  }
 });
